8c9ebf2c4050ed07f5cf4e5d3017b84d073544da
Your Name <wildheartss@tutamail.com>
2025-04-18 21:58:41 +0300

    added react jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 const App = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <div className="app">
+      <div className="navbar">
 
-export default App
+      </div>
+      <div className="main">
+
+      </div>
+      <div className='footer'></div>
+    </div>
+  );
+};
+
+export default App;

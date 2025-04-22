dcf89261828b2c59b016ddf12355edaf2f0df593
Your Name <125804179+wildheartt@users.noreply.github.com>
2025-04-22 15:46:17 +0300

    added services and store.js

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,10 @@
+// const options = {
+//   method: 'GET',
+//   hostname: 'cryptocurrency-news2.p.rapidapi.com',
+//   port: null,
+//   path: '/v1/cryptodaily',
+//   headers: {
+//     'x-rapidapi-key': '9fc0d80454mshc72e48cf036fca3p1d138cjsn3729198bcc3d',
+//     'x-rapidapi-host': 'cryptocurrency-news2.p.rapidapi.com',
+//   },
+// };

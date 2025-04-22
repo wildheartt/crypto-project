747128921e157cadbdafce4197000751095365de
Your Name <125804179+wildheartt@users.noreply.github.com>
2025-04-22 19:14:33 +0300

    added api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './app/store';
 import 'antd/dist/reset.css';
-
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -11,7 +12,9 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </StrictMode>,
 );
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,3 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+const cryptoApiHeaders = {
+  'X-RapidAPI-Key': '9fc0d80454mshc72e48cf036fca3p1d138cjsn3729198bcc3d',
+  'X-RapidAPI-Host': 'cryptocurrency-news2.p.rapidapi.com',
+};
+const baseUrl = 'https://cryptocurrency-news2.p.rapidapi.com/v1/cryptodaily';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+  }),
+});
 // const options = {
 //   method: 'GET',
 //   hostname: 'cryptocurrency-news2.p.rapidapi.com',
